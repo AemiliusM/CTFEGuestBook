879bf57bbbc25e4fd2c759ebdd41ef6d3f29c7ee
Aemilius Morgan <aemiliusmorgan@gmail.com>
2022-01-03 16:16:12 -0800

    added useForm and set up state on authentication

---
+++ src/hooks/useForm.js
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+
+export function useForm(inputs = {}) {
+  const [formState, setFormstate] = useState(inputs);
+
+  const handleFormChange = (event) => {
+    const { name, value } = event.target;
+    setFormstate((prevState) => {
+      return { ...prevState, [name]: value };
+    });
+  };
+  return { formState, handleFormChange };
+}
--- src/views/Auth/Authentication.jsx
+++ src/views/Auth/Authentication.jsx
@@ -1,3 +1,16 @@
+import { useState } from 'react';
+import { useHistory, useLocation } from 'react-router-dom';
+import { useAuth } from '../../hooks/useAuth';
+import { useForm } from '../../hooks/useForm';
+
+export default function Login() {
+  const history = useHistory();
+  const location = useLocation();
+  const auth = useAuth();
+  const { formState, handleFormChange } = useForm({ email: '', password: '' });
+  const [error, setError] = useState(null);
+}
+
 <fieldset className="w-1/4 border p-4">
   <legend>Sign In</legend>
   <form className="grid grid-cols-[1fr_2fr] grid-rows-3">
