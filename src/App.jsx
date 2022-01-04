00c8e216f3e3c149645cde6fcfe33755345f5cb8
Aemilius Morgan <aemiliusmorgan@gmail.com>
2022-01-03 17:30:17 -0800

    added paths to private route and am getting history location undefined, I tam not sure what the issue is at the moment

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
+import { Router, Switch, Route } from 'react-router';
+import { ProvideAuth } from './context/ProvideAuth';
 import Home from './views/Home/Home';
 import Layout from './views/Layout/Layout.jsx';
+import Login from './views/Auth/Authentication';
+import PrivateRoute from './components/PrivateRoute/PrivateRoute';
 
 function App() {
   return (
-    <Layout>
-      <Home />
-    </Layout>
+    <ProvideAuth>
+      <Router>
+        <Switch>
+          <Layout>
+            <Route exact path="/login">
+              <Login />
+            </Route>
+            <PrivateRoute exact path="/">
+              <Home />
+            </PrivateRoute>
+          </Layout>
+        </Switch>
+      </Router>
+    </ProvideAuth>
   );
 }
 
--- src/views/Auth/Authentication.jsx
+++ src/views/Auth/Authentication.jsx
@@ -9,37 +9,64 @@ export default function Login() {
   const auth = useAuth();
   const { formState, handleFormChange } = useForm({ email: '', password: '' });
   const [error, setError] = useState(null);
-}
 
-<fieldset className="w-1/4 border p-4">
-  <legend>Sign In</legend>
-  <form className="grid grid-cols-[1fr_2fr] grid-rows-3">
-    <label htmlFor="username" className="self-center text-right">
-      Username
-    </label>
-    <input
-      id="username"
-      type="text"
-      name="username"
-      className="border p-2 m-3"
-      required
-    />
-    <label htmlFor="password" className="self-center text-right">
-      Password
-    </label>
-    <input
-      id="password"
-      type="password"
-      name="password"
-      className="border p-2 m-3"
-      required
-    />
-    <button
-      type="submit"
-      className="h-12 self-center bg-black text-white p-1 font-bold col-span-2"
-    >
-      Sign In
-    </button>
-  </form>
-  <p className="text-center text-red-500 font-bold">Error message goes here</p>
-</fieldset>;
+  const { from } = location.state || { from: { pathname: '/' } };
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    const loginWorked = auth.login(formState.email, formState.password);
+
+    if (loginWorked) {
+      history.replaceState(from.pathname);
+    } else {
+      setError('incorrect email or password');
+    }
+  };
+
+  return (
+    <>
+      <fieldset className="w-1/4 border p-4">
+        <legend>Sign In</legend>
+        <form
+          onSubmit={handleLogin}
+          className="grid grid-cols-[1fr_2fr] grid-rows-3"
+        >
+          <label htmlFor="username" className="self-center text-right">
+            Username
+          </label>
+          <input
+            id="username"
+            type="text"
+            name="username"
+            value={formState.email}
+            onChange={handleFormChange}
+            className="border p-2 m-3"
+            required
+          />
+          <label htmlFor="password" className="self-center text-right">
+            Password
+          </label>
+          <input
+            id="password"
+            type="password"
+            name="password"
+            value={formState.password}
+            onChange={handleFormChange}
+            className="border p-2 m-3"
+            required
+          />
+          <button
+            type="submit"
+            className="h-12 self-center bg-black text-white p-1 font-bold col-span-2"
+          >
+            Sign In
+          </button>
+        </form>
+        <p className="text-center text-red-500 font-bold">
+          <p>{error}</p>
+        </p>
+      </fieldset>
+      ;
+    </>
+  );
+}
